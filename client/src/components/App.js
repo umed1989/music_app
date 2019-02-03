e40b1e35e866bc91f1b83e7798047c86a335d184
umed tolibi <tolibiumed@gmail.com>
2019-02-03 17:11:31 -0500

    Initial Commit

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import TopBar from "./TopBar";
+import AlbumsContainer from "./AlbumsContainer";
+
+import "../styles/App.css";
+
+const App = () => (
+  <div className="ui grid">
+    <TopBar />
+    <div className="spacer row" />
+    <div className="row">
+      <Route path="/albums" component={AlbumsContainer} />
+    </div>
+  </div>
+);
+
+export default App;
